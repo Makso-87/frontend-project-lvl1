47a777bce97c2ba72a3ced52d257a98749b72659
Makso-87 <makso16108@gmail.com>
2020-01-18 20:30:21 +0300

    Добавил пустую строку в Makefile

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const userNameQuestion = () => {
-  const answer = readlineSync.question('Могу я спросить Ваше имя? ');
+  const answer = readlineSync.question('Могу я спросить Ваше имя, товарищ? ');
   console.log(`Привет, ${answer}!`);
 };
 
